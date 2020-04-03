c28e460259985f4343773a97dc98bc9ca0922f86
Matthew Schrepel <matthew@extend.com>
2020-04-02 19:57:37 -0700

    Only work with body

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ var router = require('express').Router()
 
 router.get('/', (req, res) => res.send('Yup. This is the server!').status(200))
 router.post('/sms', (req, res) => {
-    console.log('Request looks like:', req.body, req)
+    console.log('Request looks like:', req.body)
     res.sendStatus(200)
 })
 
