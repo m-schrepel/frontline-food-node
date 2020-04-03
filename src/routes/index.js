626b651791e22404b80f821c9600a6ac954f4ed2
Matthew Schrepel <matthew@extend.com>
2020-04-02 17:48:09 -0700

    Testing pm2 setup

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
 var router = require('express').Router();
 
-router.get('/', (req, res) => res.send('Yup. This is the server.').status(200))
+router.get('/', (req, res) => res.send('Yup. This is the server!').status(200))
 
 module.exports = router
