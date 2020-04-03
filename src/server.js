0e48203dc957425b50f32617a9a4bebcb551686f
Matthew Schrepel <matthew@extend.com>
2020-04-02 20:00:57 -0700

    parsers before routes, 'cuz duh

--- src/server.js
+++ src/server.js
@@ -3,11 +3,12 @@ const router = require('./routes')
 const bodyParser = require('body-parser')
 
 const app = express()
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 // Nginx reverse proxies 3000 to 80
 const port = 3000
 // routes entrypoint
 app.use(router)
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
 
 app.listen(port, () => console.log(`Server started on ${port}`))
