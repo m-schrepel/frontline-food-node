54683de9aa6f300b3969513910441a1f0daa3f86
Matthew Schrepel <matthew@extend.com>
2020-04-03 09:59:29 -0700

    add slack code to test webhooks

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,42 @@
-var router = require('express').Router()
+const router = require('express').Router()
+const fetch = require('node-fetch')
 
 router.get('/', (req, res) => res.send('Yup. This is the server!').status(200))
 router.post('/sms', (req, res) => {
-    console.log('Request looks like:', req.body)
+    fetch(process.env.SLACK_HOOK, {
+        method: 'post',
+        body: JSON.stringify({
+            blocks: [
+                {
+                    type: 'section',
+                    text: {
+                        type: 'mrkdwn',
+                        text: `:iphone: Incoming SMS :iphone:`
+                    }
+                },
+                {
+                    type: 'divider'
+                },
+                {
+                    type: 'section',
+                    text: {
+                        type: 'mrkdwn',
+                        text: `*Message says*: ${req.body.Body}`
+                    }
+                },
+                {
+                    type: 'section',
+                    text: {
+                        type: 'mrkdwn',
+                        text: `[With image](${req.body.MediaUrl0})`
+                    }
+                },
+
+            ]
+        })
+    })
     res.sendStatus(200)
 })
 
 module.exports = router
+
