dc04b1157217f42f709da350848bb96ba31436ad
Matthew Schrepel <matthew@extend.com>
2020-04-11 14:51:51 -0700

    Start moving around code, testing gdrive

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,52 +1,7 @@
 const router = require('express').Router()
-const fetch = require('node-fetch')
+const smsController = require('./sms-controller')
 
-router.get('/', (req, res) => res.send('Yup. This is the server!').status(200))
-router.get('/oauth2callback', (req, res) => {
-    const code = req.query.code;
-    client.getToken(code, (err, tokens) => {
-        if (err) {
-            console.error('Error getting oAuth tokens:');
-            throw err;
-        }
-        client.credentials = tokens;
-        res.send('Authentication successful! Please return to the console.');
-    });
-})
-router.post('/sms', async (req, res) => {
-    console.log(process.env.SLACK_HOOK)
-    await fetch(process.env.SLACK_HOOK, {
-        method: 'post',
-        body: JSON.stringify({
-            blocks: [
-                {
-                    type: 'section',
-                    text: {
-                        type: 'mrkdwn',
-                        text: `:iphone: Incoming SMS :iphone:`
-                    }
-                },
-                {
-                    type: 'divider'
-                },
-                {
-                    type: 'section',
-                    text: {
-                        type: 'mrkdwn',
-                        text: `*Message says*: ${req.body.Body}`
-                    }
-                },
-                {
-                    type: 'image',
-                    image_url: `${req.body.MediaUrl0}`,
-                    alt_text: 'Image from SMS sender'
-                },
-
-            ]
-        })
-    })
-    return res.sendStatus(200)
-})
+router.post('/sms', smsController)
 
 module.exports = router
 
